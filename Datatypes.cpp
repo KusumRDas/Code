8cda3a7af2a23ff1ea130254e50a9b6052c914f9
KusumRDas <kusumkukif@gmail.com>
2022-08-01 19:20:25 +0530

    Addition

--- Datatypes.cpp
+++ Datatypes.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	char grade = 'A';
-	string phrase = "Kusum";
+	string phrase = "Kusum";   //Insert Name
 	int age = 50;
 	double gpa = 2.3;
 	bool isMale = false;
---
+++ Working with Math Functions.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+	cout << pow(2,5) << endl;
+	cout << sqrt(36) << endl;
+	cout << round(8.5) << endl;
+	cout << ceil(8.7) << endl;
+	cout << floor(8.7) << endl;
+	cout << fmax(3, 10) << endl;
+	cout << fmin(3, 10) << endl;
+	
+	
+	return 0;
+}
