e94056090f9264239ad20ba4ed1cb9c69c9c3c97
kusum <kusumkukif@gmail.com>
2022-08-01 19:02:11 +0530

    Code uploaded

---
+++ Change name & variable.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string characterName = "TinTin.";
+	int characterAge;
+	characterAge = 18;
+	cout<<"There once was a man named " << characterName ;
+	cout<<"He was " << characterAge << " years old.";
+	
+	characterName = "Mike";
+	cout<<"He liked his name " << characterName ;
+	cout<<"But he did not liked being " << characterAge ;
+	cout<<"Instead he wants to be 25.";
+	return 0;
+}
